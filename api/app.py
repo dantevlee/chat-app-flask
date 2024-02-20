05c9d7c7e4fde9116e8b79e553c7c11d6cd5dedd
dantevlee <dante.leeseberg@gmail.com>
2024-02-19 20:20:52 -0600

    app changes

--- api/app.py
+++ api/app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
 from flask_smorest import Api
+from flask_cors import CORS
+from flask_socketio import SocketIO
 from dotenv import load_dotenv
 
 from database.db import db
@@ -10,6 +12,9 @@ from resources.users import blp as UsersBlueprint
 def create_app(db_url=None):
   app = Flask(__name__)
   load_dotenv()
+  CORS(app, resources={r'/*': {'origins': '*'}})
+  
+  socketio = SocketIO(app, cors_allowed_origins='*')
   
   app.config["API_TITLE"] = "Chat App REST API"
   app.config["API_VERSION"] = "v1"
